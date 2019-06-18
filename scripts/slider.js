a82f62d085ae6264f0171f8f245770a24f0b3a0b
Nikoloz <47790698+n1kalai@users.noreply.github.com>
2019-06-18 12:40:47 +0400

    First Page Ready

---
+++ scripts/slider.js
@@ -0,0 +1,10 @@
+ $(document).ready(function() {
+            $('.secMain').bxSlider({
+				auto: true,
+				autoPlay: 5000,
+				speed: 1000,
+				tickerHover: true,
+                pager: false
+				 
+			});
+        });
