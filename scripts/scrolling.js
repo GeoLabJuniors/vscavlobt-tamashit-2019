24f166a90078754b54fc29900a6a56c3bf208dbd
n1kalai <nikoloz.palagashvili@gmail.com>
2019-07-18 17:07:16 +0400

    zemot qvemot

---
+++ scripts/scrolling.js
@@ -0,0 +1,34 @@
+window.onscroll = function() {scroll()};
+
+function scroll() {
+    if (document.body.scrollTop > 1000 || document.documentElement.scrollTop > 1000) {
+         
+        document.getElementById("myBtn2").style.display = "none";
+         document.getElementById("myBtn").style.display = "block";
+            document.getElementById("myBtn").style.transform = "translate(180deg)";
+        
+    } else  {
+        document.getElementById("myBtn").style.display = "none";
+         document.getElementById("myBtn2").style.display = "block";
+       
+        }
+    
+    
+}
+
+function topFunction() {
+ document.body.scrollTop = 0; // For Safari
+ document.documentElement.scrollTop = 0; // For Chrome, Firefox, IE and Opera
+}
+
+function bottomFunction() {
+    var number = 200
+    
+   
+ document.body.scrollTop =  number// For Safari
+ document.documentElement.scrollTop =  number  // For Chrome, Firefox, IE and Opera
+    number = number + 200
+    console.log(number)
+    
+    
+}
